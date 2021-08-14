229dd60d027e3752a355aa26e155df1735d142ec
Fernando Valdes <fernando.valdes@fashionspark.com>
2021-08-14 19:41:08 -0400

    correcion doble input

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 
 // Numero "Random" que no es random 😅 aun no aprendo a crear uno bien.
 randomNumber = 7
-// Input Numero del jugagador
-let userNumber = parseInt(prompt('Ingresa un numero entero entre 1 y 10' + ' [ APRETE 0 para salir]'))
 
+i=1
 
-while(userNumber > 0 && userNumber < 11){
+while(i >0){
+    // Input Numero del jugagador
     let userNumber = parseInt(prompt('Ingresa un numero entero entre 1 y 10' + ' [ APRETE 0 para salir]'))
     if (userNumber == 0){
         alert('saliste... Apreta f5 para reiniciar')
